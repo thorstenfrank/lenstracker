d9ca0221d9f5e2bd79b54c3eba7d12de3557d2d4
Thorsten Frank <thorsten@tfsw.de>
2020-08-09 20:37:08 +0200

    basic layout and labels plus dummy buttons

--- app/src/main/java/de/tfsw/lenstracker/MainActivity.kt
+++ app/src/main/java/de/tfsw/lenstracker/MainActivity.kt
@@ -3,41 +3,64 @@ package de.tfsw.lenstracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.TemporalAmount
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG: String = "MainActiviy"
-
-    var lensData: LensData = LensData()
+    private val logTag = "MainActiviy"
+    private val lensData = LensData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        readLensData()
         setContentView(R.layout.activity_main)
+
+        readLensData()
+
+        val inUseSinceText: TextView = findViewById(R.id.textInUseSince)
+
+
+        if (lensData.dateOpened == null) {
+            inUseSinceText.text = getString(R.string.noLensesInUse)
+        } else {
+            val numberOfDays = Period.between(lensData.dateOpened, LocalDate.now()).days
+            inUseSinceText.text = resources.getQuantityString(
+                R.plurals.textInUseSince,
+                numberOfDays,
+                lensData.dateOpened?.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)),
+                numberOfDays
+            )
+        }
+
+        val timesUsedText: TextView = findViewById(R.id.textTimesUsed)
+        val timesUsed = lensData.timesUsed.size
+        timesUsedText.text = resources.getQuantityString(R.plurals.textTimesUsed, timesUsed, timesUsed)
     }
 
     override fun onPause() {
-        Log.d(TAG, "*** onPause")
+        Log.d(logTag, "*** onPause")
         super.onPause()
     }
 
     override fun onStop() {
-        Log.d(TAG, "*** onStop")
+        Log.d(logTag, "*** onStop")
         super.onStop()
     }
 
     override fun onDestroy() {
-        Log.d(TAG, "*** onDestroy")
+        Log.d(logTag, "*** onDestroy")
         super.onDestroy()
     }
 
-    fun readLensData() {
-        lensData.dateOpened = LocalDate.of(2020, 8, 1)
+    private fun readLensData() {
+        lensData.dateOpened = LocalDate.of(2020, 8, 2)
         lensData.timesUsed.add(LocalDate.of(2020, 8, 1))
         lensData.timesUsed.add(LocalDate.of(2020, 8, 3))
         lensData.timesUsed.add(LocalDate.of(2020, 8, 4))
         lensData.timesUsed.add(LocalDate.of(2020, 8, 7))
-        // TODO actually read from storage
     }
 }
