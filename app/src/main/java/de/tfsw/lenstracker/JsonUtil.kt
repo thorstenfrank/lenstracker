bf5937f57754710a67d2040b2c5ddf9b33266e6b
Thorsten Frank <thorsten@tfsw.de>
2020-08-11 02:07:43 +0200

    removes debug logging

--- app/src/main/java/de/tfsw/lenstracker/JsonUtil.kt
+++ app/src/main/java/de/tfsw/lenstracker/JsonUtil.kt
@@ -1,6 +1,5 @@
 package de.tfsw.lenstracker
 
-import android.util.Log
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
@@ -10,7 +9,6 @@ import java.util.function.Consumer
 object JsonUtil {
 
     private const val FILE_NAME = "lensdata.json"
-    private const val LOG_TAG = "JsonUtil"
     private const val KEY_VERSION = "version"
     private const val KEY_DATE_OPENED = "dateOpened"
     private const val KEY_TIMES_USED = "timesUsed"
@@ -20,12 +18,9 @@ object JsonUtil {
         val file = File(filesDir, FILE_NAME)
 
         if (file.exists()) {
-            Log.d(LOG_TAG, "Storage file exists")
-
             val json = JSONObject(String(file.readBytes()))
 
-            val version = json.getInt(KEY_VERSION)
-            Log.d(LOG_TAG, "Storage file version: $version")
+            //val version = json.getInt(KEY_VERSION)
 
             val dateOpened = json.getString(KEY_DATE_OPENED)
             lensData.dateOpened = LocalDate.parse(dateOpened)
@@ -36,14 +31,10 @@ object JsonUtil {
                     lensData.timesUsed.add(LocalDate.parse(timesUsed.getString(i)))
                 }
             }
-        } else {
-            Log.d(LOG_TAG, "No storage file found")
         }
     }
 
     fun saveLensDataToFile(filesDir: File, lensData: LensData) {
-        Log.d(LOG_TAG, "Saving to storage...")
-
         if (lensData.dateOpened != null) {
             val file = File(filesDir, FILE_NAME)
 
@@ -55,11 +46,7 @@ object JsonUtil {
             lensData.timesUsed.forEach(Consumer { t -> timesUsedJson.put(t.toString()) })
             json.put(KEY_TIMES_USED, timesUsedJson)
 
-            Log.d(LOG_TAG, "Now writing: " + json.toString())
-
             file.writeBytes(json.toString().toByteArray())
-        } else {
-            Log.d(LOG_TAG, "No data to write, aborting")
         }
     }
 }
